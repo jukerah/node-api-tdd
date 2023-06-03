ec5338d8494d40bf08ab7fcb7c1c8e1783ad96ac
jukerah <juka_mebaj@hotmail.com>
2023-06-03 01:39:49 -0300

    fix: fixed conflitcts

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import "express-async-errors"
+import cors from "./libs/cors"
+import rateLimit from "./libs/express-rate-limit"
+import router from "./infra/routes"
+import appMiddleware from "./application/middlewares/app-middleware"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors)
+app.use(rateLimit)
+app.use(router)
+app.use(appMiddleware)
+
+export { app }
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,4 @@
-import express, { NextFunction, Request, Response, Router } from "express"
-
-const app = express()
-app.use(express.json())
-
-const router = Router()
-
-router.get("/test", (req: Request, res: Response) => {
-	return res.json({ status: "Hello World!" })
-})
-
-app.use(router)
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof Error) return res.status(400).json({
-    error: err.message
-  });
-
-  return res.status(500).json({
-    status: "error",
-    message: "Internal server error."
-  });
-});
+import { app } from "./app"
 
 const port = process.env.PORT || 3030
 
