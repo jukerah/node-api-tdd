30cfc8e218507c591dd5e219a65697354a3da06a
jukerah <juka.mebaj@gmail.com>
2023-08-10 00:40:19 -0300

    chore: change users model.

--- prisma/migrations/20230804022342_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Products" (
-    "productId" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "amount" INTEGER NOT NULL
-);
